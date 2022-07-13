99fc1b7011901cc63acaa2c75a850f9d3208052e
3rdAnimal <41023624+3rdAnimal@users.noreply.github.com>
2022-07-12 19:38:57 -0700

    Script error fixes
    Looks like I deleted some declarations with a highlight and typing "/"  Fixed.

--- Shaders/ShaderGraph/Beams/ChromaticStrings/VFX_Beam_TEMPScript_A.cs
+++ Shaders/ShaderGraph/Beams/ChromaticStrings/VFX_Beam_TEMPScript_A.cs
@@ -1,5 +1,13 @@
-\using System.Collections;
+// resources for curve
+// https://forum.unity.com/threads/generating-dynamic-parabola.211681/#post-1426169
+// https://old.reddit.com/user/Sanketpanda/comments/f475vn/make_a_curved_line_using_unity3d_line_renderer/
+// https://www.youtube.com/watch?v=Xo3n4W3QbWM
+
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+
+
 
     //Anchors Line Renderer positions to corresponding GameObjects in the Unity Editor
     [ExecuteInEditMode]
@@ -7,7 +15,21 @@ using System.Collections.Generic;
     {
         public List<GameObject> attachedObjects;
         public LineRenderer lineRenderer;
-     
+
+/*    Vector3 SampleParabola(Vector3 start, Vector3 end, float height, float t, Vector3 outDirection)
+    {
+        float parabolicT = t * 2 - 1;
+        //start and end are not level, gets more complicated
+        Vector3 travelDirection = end - start;
+        Vector3 levelDirection = end - new Vector3(start.x, end.y, start.z);
+        Vector3 right = Vector3.Cross(travelDirection, levelDirection);
+        Vector3 up = outDirection;
+        Vector3 result = start + t * travelDirection;
+        result += ((-parabolicT * parabolicT + 1) * height) * up.normalized;
+        return result;
+    }*/
+
+
     #if UNITY_EDITOR
         void Update()
         {
